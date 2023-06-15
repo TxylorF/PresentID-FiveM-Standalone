31c2d3d62732479139339980f3df5821d8d33319
Taylor <136265418+TxylorF@users.noreply.github.com>
2023-06-15 12:15:13 +0100

    ASCII Art added to the script

--- Server.lua
+++ Server.lua
@@ -1,5 +1,23 @@
 -- Server-side FiveM script for /id command
 
+local logo = {
+    [[_____  _____  ______  _____ ______ _   _ _______ _____ _____  ]],
+    [[|  __ \|  __ \|  ____|/ ____|  ____| \ | |__   __|_   _|  __ \ ]],
+    [[| |__) | |__) | |__  | (___ | |__  |  \| |  | |    | | | |  | |]],
+    [[|  ___/|  _  /|  __|  \___ \|  __| | . ` |  | |    | | | |  | |]],
+    [[| |    | | \ \| |____ ____) | |____| |\  |  | |   _| |_| |__| |]],
+    [[|_|    |_|  \_\______|_____/|______|_| \_|  |_|  |_____|_____/ ]],
+
+    "",
+    "PRESENTID by TxylorF (Discord)",
+    "For the latest version please check",
+    "https://github.com/TxylorF/PresentID-FiveM-Standalone/tree/main"
+}
+
+for _, line in ipairs(logo) do
+    print(line)
+end
+
 AddEventHandler('chatMessage', function(source, name, message)
     if message:sub(1, 3) == '/id' then
         local playerCoords = GetEntityCoords(GetPlayerPed(source))
@@ -9,7 +27,7 @@ AddEventHandler('chatMessage', function(source, name, message)
             local targetCoords = GetEntityCoords(GetPlayerPed(playerId))
             local distance = #(playerCoords - targetCoords)
 
-            if distance <= 20.0 then -- Change 20 to your liking, this is in metres, you can also just get rid of the if and just add it to the main if at the top if you want it to be global :)
+            if distance <= 20.0 then
                 local additionalText = message:sub(4)
                 local serverIdText = "^4[" .. source .. "]"
                 local finalMessage = string.format("^4%s ^7%s ^7shows ID: ^7%s", name, serverIdText, additionalText)
